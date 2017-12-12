95026b753abdb905259b969768ba21e18b99efed
dionwu110 <kevin110023@yahoo.com>
2017-12-12 01:49:07 -0800

    First Commit
    Added Unity Project to GitHub

---
+++ 343 Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        if (autoLoadNextLevelAfter <= 0)
+        {
+            Debug.Log("Level auto load disabled, use a positive number in seconds");
+        }
+        else
+        {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
---
+++ 343 Project/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+    private bool alreadyPlaying = false;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destory on load: " + name);
+    }
+
+    void Start()
+    {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.volume = PlayerPrefsManager.GetMasterVolume();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+        Debug.Log("Playing clip: " + thisLevelMusic);
+
+    if (alreadyPlaying = true)
+        {
+
+        }
+    else if (!alreadyPlaying && thisLevelMusic)
+        { // If there's some music attached
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+            alreadyPlaying = true;
+        }
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+}
